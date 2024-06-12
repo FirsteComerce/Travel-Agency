aaee33dc4c03f03dea7b3f1a07b0f3f5c1b120ce
unknown <azzatrk01@gmail.com>
2024-06-12 16:02:19 +0100

    server done

--- Travel Agency/server/controllers/hotel.controller.js
+++ Travel Agency/server/controllers/hotel.controller.js
@@ -1,14 +1,14 @@
-const Hotels = require ('../database-mongodb/hotel.model.js')
+const Hotels = require('../database-mongodb/hotel.model.js');
 
-const selectAllHotels = function (res) {
+const selectAllHotels = function (req, res) {
   Hotels.find({})
     .then(hotels => {
-      console.log("done");
-      res.send("done");
+      console.log(hotels);
+      res.json(hotels); 
     })
     .catch(error => {
-      console.log("error try again");
-      res.send("error try again");
+      console.error("Error fetching hotels:", error.message);
+      res.status(500).json({ error: "An error occurred while fetching hotels data" });
     });
 };
 
@@ -16,12 +16,12 @@ const addHotels = function (req, res) {
   const { price, type, Capacite, Name, Etoile, Description, Image } = req.body;
   Hotels.create({ price, type, Capacite, Name, Etoile, Description, Image })
     .then(hotel => {
-      console.log("done");
-      res.send("done");
+      console.log("Hotel added:", hotel);
+      res.json(hotel);
     })
     .catch(error => {
-      console.log("error try again");
-      res.send("error try again");
+      console.error("Error adding hotel:", error.message);
+      res.status(500).json({ error: "An error occurred while adding a hotel" });
     });
 };
 
@@ -30,12 +30,12 @@ const updateHotels = function (req, res) {
   const { price, type, Capacite, Name, Etoile, Description, Image } = req.body;
   Hotels.findByIdAndUpdate(id, { price, type, Capacite, Name, Etoile, Description, Image }, { new: true })
     .then(hotel => {
-      console.log("done");
-      res.send("done");
+      console.log("Hotel updated:", hotel);
+      res.json(hotel);
     })
     .catch(error => {
-      console.log("error try again");
-      res.send("error try again");
+      console.error("Error updating hotel:", error.message);
+      res.status(500).json({ error: "An error occurred while updating the hotel" });
     });
 };
 
@@ -43,12 +43,12 @@ const deleteHotels = function (req, res) {
   const { id } = req.params;
   Hotels.findByIdAndDelete(id)
     .then(() => {
-      console.log("done");
-      res.send("done");
+      console.log("Hotel deleted successfully");
+      res.json({ message: "Hotel deleted successfully" });
     })
     .catch(error => {
-      console.log("error try again");
-      res.send("error try again");
+      console.error("Error deleting hotel:", error.message);
+      res.status(500).json({ error: "An error occurred while deleting the hotel" });
     });
 };
 
@@ -57,15 +57,15 @@ const searchHotels = function (req, res) {
   Hotels.find({ Name: Name })
     .then(hotels => {
       if (hotels.length === 0) {
-        console.log("error try again");
-        return res.send("error try again");
+        console.log("No hotels found with the given name:", Name);
+        return res.status(404).json({ message: "No hotels found with the given name" });
       }
-      console.log("done");
-      res.send("done");
+      console.log("Hotels found:", hotels);
+      res.json(hotels);
     })
     .catch(error => {
-      console.log("error try again");
-      res.send("error try again");
+      console.error("Error searching hotels:", error.message);
+      res.status(500).json({ error: "An error occurred while searching hotels" });
     });
 };
 
